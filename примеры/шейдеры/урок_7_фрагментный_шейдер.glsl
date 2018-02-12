d68687d4b80584042459b3511a13b84bae3c5125
Dobrosvet <dimaprokofev@yandex.ru>
2018-02-12 14:51:28 +0300

    Добавлено фоновое и направленное освещение #10, #1
    - добавлена возможность использования нормалей
    - добавлено фоновое и направленное освещение
    - добавлена возможность включать и выключать свет для текстурированных
      моделей
    - добавлен "Урок 7. Основы фонового и направленного освещения" в
      качестве примера
    - исправлен урок 6, убрана лишняя строчка

---
+++ примеры/шейдеры/урок_7_фрагментный_шейдер.glsl
@@ -0,0 +1,11 @@
+precision mediump float;
+
+uniform sampler2D uniform_sempler;
+
+varying vec2 koordinaty_tekstury;
+varying vec3 sila_osveshheniya;
+
+void main(void) {
+    vec4 cvet_teksury = texture2D(uniform_sempler, vec2(koordinaty_tekstury.s, koordinaty_tekstury.t));
+    gl_FragColor = vec4(cvet_teksury.rgb * sila_osveshheniya, cvet_teksury.a);
+}
